03cf6a5c8e4f8bd4467e9fc0203e62b0f5d02990
PaulEgopyork23 <152753605+PaulEgopyork23@users.noreply.github.com>
2024-10-23 13:15:37 +0100

    Add files via upload

---
+++ home.js
@@ -0,0 +1,43 @@
+// let h2 = document.getElementById("two")
+// let p = document.getElementById("three")
+
+// let h22 = document.getElementById("two1")
+// let pp = document.getElementById("three1")
+
+// let h222 = document.getElementById("two2")
+// let ppp = document.getElementById("three2")
+
+// ADDING EVENT LISTENER /
+// h22.addEventListener("click", ()=>{
+//     h22.classList.add("two")
+//     pp.style.display = "block"
+
+//     h2.classList.remove("two")
+//     p.style.display = "none"
+
+//     h222.classList.remove("two")
+//     ppp.style.display = "none"
+    
+// })
+
+// h222.addEventListener("click",()=>{
+//     h222.classList.add("two")
+//     ppp.style.display = "block"
+
+//     h22.classList.remove("two")
+//     pp.style.display = "none"
+
+//     h2.classList.remove("two")
+//     p.style.display = "none"
+// })
+
+// h2.addEventListener("click",()=>{
+//     h2.classList.add("two")
+//     p.style.display = "block"
+
+//     h22.classList.remove("two")
+//     pp.style.display = "none"
+
+//     h222.classList.remove("two")
+//     ppp.style.display = "none"
+// })
